3af722f9df84cadee230f22117656f42dfda6119
Mike Rosset <mike.rosset@gmail.com>
2019-09-30 11:56:56 -0700

    QtWebView files now have copyright headers

--- application/qt-web-view.cpp
+++ application/qt-web-view.cpp
@@ -1,3 +1,23 @@
+/*
+ * qt-web-view.cpp
+ * Copyright (C) 2017-2019 Michael Rosset <mike.rosset@gmail.com>
+ *
+ * This file is part of giqt
+ *
+ * giqt is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ * giqt is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "qt-web-view.h"
 #include "util.h"
 #include <QLabel>
